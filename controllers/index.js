8223daa9291c5964bcdf7c656ae8e4512909567b
SoniiPP <sonypoudel30@gmail.com>
2024-06-13 19:53:09 +1000

    added index file

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const homeRoutes = require('../controllers/home-routes');
+const movieRoutes = require('../controllers/api/movie-routes');
+const reviewRoutes = require('../controllers/api/review-routes');
+const userRoutes = require('../controllers/api/user-routes');
+
+router.use('/', homeRoutes);
+router.use('/api/movies', movieRoutes);
+router.use('/api/reviews', reviewRoutes);
+router.use('/api/users', userRoutes);
+
+module.exports = router;
