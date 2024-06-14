72b955b0652ef2b6c2fbde7c0d66a5a99bffc52b
SoniiPP <sonypoudel30@gmail.com>
2024-06-14 23:20:27 +1000

    added some user-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,31 @@
-const router = require(`express`).Router();
-const {Movie} = require(`../models`);
+// /controllers/home-routes.js
+const router = require('express').Router();
+const { Movie } = require('../models');
 
-
-router.get(`/`, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const movieData = await Movie.findAll({
-    }); 
-    
-    const movies = movieData.map((data) => data.get({plain: true}));
-
-    // console.log(posts);
-
-    res.render(`homepage`, {movies});
-
-  } catch (error) {
-    res.status(500).json(error);
+    const movieData = await Movie.findAll();
+    const movies = movieData.map((movie) => movie.get({ plain: true }));
+    res.render('homepage', { movies, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
   }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
 });
 
 module.exports = router;
